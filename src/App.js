29790648ba705844b6fb604c0dc165012b902fb4
ponmalini <ponmaliniprasanna@gmail.com>
2024-09-12 11:09:55 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Fruits from './components/Fruits'
+function App() {
+  return (
+    <div className="App">
+     <Fruits/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Fruits.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './fruits.css'
+function Fruits() {
+  return (
+    <div> 
+        <h1>FRUITS</h1>
+        <div className="img"></div>
+        <img src="https://media.istockphoto.com/id/139496979/photo/assortment-of-fruits-and-vegetables-background.jpg?s=612x612&w=0&k=20&c=Tns4-67GV8LoJoN9YYwRM9PhpYKQ4kfbWg70_NJc9L8=">
+        </img>  
+        <p>Fruits are a delightful and essential part of a healthy diet, each offering unique flavors and nutritional benefits. 
+          Apples are incredibly versatile, perfect for everything from fresh snacks to delicious pies. Bananas provide a convenient source of potassium, supporting heart health and muscle function. Oranges, bursting with vitamin C, are excellent for boosting the immune system. Strawberries, with their antioxidant-rich properties, not only taste great but also help protect the body from free radicals. Mangoes, often hailed as the "king of fruits," offer a tropical sweetness and a wealth of vitamins. Grapes are equally versatile, enjoyed fresh, dried as raisins, or in the form of wine. Pineapples add a tropical twist to any dish and contain bromelain, which aids digestion. Kiwis, with their vibrant green flesh and unique texture, are packed with vitamin C and fiber. Avocados, although technically fruits, are celebrated for their creamy texture and healthy fats. Finally, blueberries, small but potent, are rich in antioxidants that contribute
+         to overall health. Together, these fruits not only enhance our meals but also support our well-being in various ways.</p> 
+    </div>
+  )
+}
+
+export default Fruits;
